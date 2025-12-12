9999104c61676980d34a7b8c190334a17944a368
EC2 Default User <ec2-user@ip-172-31-31-216.ap-south-1.compute.internal>
2025-12-12 13:33:20 +0000

    EC2 backend config + API fixes + working version

---
+++ backend/upskillwaybackend-twilio/src/types/shim.d.ts
@@ -0,0 +1,7 @@
+declare module 'swagger-jsdoc';
+declare module 'swagger-ui-express';
+declare module 'jsonwebtoken';
+declare module 'cors';
+declare module 'bcrypt';
+declare module 'uuid';
+declare var jest: any;
